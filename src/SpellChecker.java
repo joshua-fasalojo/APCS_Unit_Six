f9333a72dc3166621adea58d63efbd419b9ac7b8
joshua-fasalojo <fasalif@ssfs.org>
2021-12-10 22:06:33 -0500

    spell checker

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -1,6 +1,30 @@
-
+import java.util.Scanner;
 
 public class SpellChecker {
+    word1();
+
+    public static int alice(int arr[], int num) {
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min = max) / 2;
+            if (arr[mid] == num)
+                return mid;
+            else if (arr[mid] > num)
+                min = mid + 1;
+            else
+                max = mid - 1;
+
+        }
+
+        public static String[] getDictionary () throws IOException {
+            Scanner input = new Scanner(System.in);
+             = input.nextLine();
+            System.out.println("Is this a word?" );
 
 
+        }
+
+    }
 }
